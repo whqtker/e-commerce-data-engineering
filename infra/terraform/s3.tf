e94be08afe06033688fdb7dc9f53d0fdcc890e9e
seonghyeok <chosh2001@naver.com>
2025-08-26 16:00:51 +0900

    chore: spark 애플리케이션 전용 IAM 생성

--- infra/terraform/s3.tf
+++ infra/terraform/s3.tf
@@ -1,8 +1,62 @@
+# Spark 애플리케이션을 위한 IAM 사용자
+resource "aws_iam_user" "spark_app_user" {
+  name = "spark-s3-data-lake-user"
+  tags = {
+    Project = "DE-Portfolio-Project"
+  }
+}
+
 # S3 버킷 이름에 사용할 랜덤 문자열
 resource "random_string" "bucket_suffix" {
   length  = 8
   special = false
   upper   = false
+}
+
+resource "aws_iam_policy" "s3_data_lake_access_policy" {
+  name        = "S3DataLakeAccessPolicy"
+  description = "Allows Spark job to write to the data lake S3 bucket"
+
+  # IAM 정책
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:PutObject", # 객체 생성/덮어쓰기
+          "s3:GetObject", # 객체 읽기
+          "s3:ListBucket", # 버킷 내 객체 목록 조회
+          "s3:DeleteObject" # 임시 파일 삭제 등
+        ],
+        Resource = [
+          aws_s3_bucket.data_lake.arn,
+          "${aws_s3_bucket.data_lake.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+# IAM 사용자와 정책 부착
+resource "aws_iam_policy_attachment" "spark_user_policy_attachment" {
+  name       = "spark-user-s3-policy-attachment"
+  users      = [aws_iam_user.spark_app_user.name]
+  policy_arn = aws_iam_policy.s3_data_lake_access_policy.arn
+}
+
+resource "aws_iam_access_key" "spark_app_user_key" {
+  user = aws_iam_user.spark_app_user.name
+}
+
+output "spark_app_user_access_key_id" {
+  value     = aws_iam_access_key.spark_app_user_key.id
+  sensitive = true
+}
+
+output "spark_app_user_secret_access_key" {
+  value     = aws_iam_access_key.spark_app_user_key.secret
+  sensitive = true
 }
 
 # S3
@@ -52,4 +106,9 @@ resource "aws_s3_bucket_lifecycle_configuration" "data_lake_lifecycle" {
       noncurrent_days = 180
     }
   }
+}
+
+output "data_lake_bucket_name" {
+  value       = aws_s3_bucket.data_lake.bucket
+  description = "The name of the created S3 data lake bucket."
 }
