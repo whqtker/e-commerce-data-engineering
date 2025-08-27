6e090ddc2d843436b7718d5977a2cabb50e9a703
seonghyeok <chosh2001@naver.com>
2025-08-27 12:23:13 +0900

    feat: DAG 정의

---
+++ batch_processor/dags/train_recommend_model_dag.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+import pendulum
+from airflow.models.dag import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+
+with DAG(
+        dag_id="train_recommendation_model_dag",
+        start_date=pendulum.datetime(2025, 8, 25, tz="Asia/Seoul"),
+        schedule="@daily", # 매일 자정
+        catchup=False,
+        tags=["recommendation", "spark", "batch"],
+) as dag:
+    submit_spark_job = SparkSubmitOperator(
+        task_id="submit_als_model_trainer",
+        conn_id="spark_default",
+        application="/opt/bitnami/spark/jobs/batch/model_trainer.py",
+        verbose=True,
+    )
